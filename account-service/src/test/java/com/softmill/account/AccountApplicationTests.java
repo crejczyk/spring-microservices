04e3da4c0333cbbbc50bcc045e38cb530f54ca2a
cezary.rejczyk <cezary.rejczyk@capsilon.com>
2018-05-11 16:15:55 +0200

    [Fix] Working on Account service.

--- account-service/src/test/java/com/softmill/account/AccountApplicationTests.java
+++ account-service/src/test/java/com/softmill/account/AccountApplicationTests.java
@@ -2,15 +2,18 @@ package com.softmill.account;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = AccountApplication.class)
+@WebAppConfiguration
 public class AccountApplicationTests {
 
 	@Test
 	public void contextLoads() {
+
 	}
 
 }
