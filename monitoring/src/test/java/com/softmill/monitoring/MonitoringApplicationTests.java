c9e8e1a625f50d2554d16cec1d9bfb8619e412c1
cezary.rejczyk <cezary.rejczyk@capsilon.com>
2018-05-29 11:13:24 +0200

    [Fix] Adding monitoring serivce

---
+++ monitoring/src/test/java/com/softmill/monitoring/MonitoringApplicationTests.java
@@ -0,0 +1,18 @@
+package com.softmill.monitoring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.softmill.monitoring.MonitoringApplication;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = MonitoringApplication.class)
+public class MonitoringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
